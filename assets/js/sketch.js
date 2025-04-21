50652f91579a26f0476d089a9be01e107e5be72c
makingdpeace <99088823+makingdpeace@users.noreply.github.com>
2025-04-21 09:06:56 +0100

    Update sketch.js

--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -3,14 +3,17 @@ let y = 100;
 let speedX = 2;
 let speedY = 3;
 let ballRadius = 25; // Radius of the ball for collision detection
-let canvas;
+let container = document.getElementById('p5-sketch-container');
 
 function setup() {
-  let container = document.getElementById('p5-sketch-container');
   canvas = createCanvas(container.offsetWidth, container.offsetHeight);
   canvas.parent('p5-sketch-container');
   noStroke();
   }
+
+function windowResized() {
+  resizeCanvas(container.offsetWidth, container.offsetHeight);
+}
 
 function draw() {
   clear(); // Keep the background transparent
@@ -23,16 +26,11 @@ function draw() {
   y += speedY;
 
   // Check if the ball hits the left or right edge
-  if (x - ballRadius < 0 || x + ballRadius > Width) {
+  if (x - ballRadius < 0 || x + ballRadius > container.offsetWidth) {
     speedX *= -1; // Reverse horizontal direction if hitting left or right
   }
 
   // Check if the ball hits the top or bottom edge
-  if (y - ballRadius < 0 || y + ballRadius > Height) {
+  if (y - ballRadius < 0 || y + ballRadius > container.offsetHeight) {
     speedY *= -1; // Reverse vertical direction if hitting top or bottom
   }
-
-function windowResized() {
-  let container = document.getElementById('p5-sketch-container');
-  resizeCanvas(container.offsetWidth, container.offsetHeight);
-}
