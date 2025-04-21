38bc93c55511b5ed57c8862f1904bfe18572d16e
makingdpeace <99088823+makingdpeace@users.noreply.github.com>
2025-04-21 08:01:15 +0100

    Create sketch.js

---
+++ assets/js/sketch.js
@@ -0,0 +1,25 @@
+let x = 100;
+let y = 100;
+let speedX = 2;
+let speedY = 3;
+
+function setup() {
+  let canvas = createCanvas(windowWidth, windowHeight);
+  canvas.parent('p5-sketch-container');
+  noStroke();
+}
+
+function draw() {
+  background(0);
+  fill(255, 0, 0);
+  ellipse(x, y, 50, 50);
+  x += speedX;
+  y += speedY;
+
+  if (x > width || x < 0) {
+    speedX *= -1;
+  }
+  if (y > height || y < 0) {
+    speedY *= -1;
+  }
+}
